972b0539cdb84d9563b970fe368586022ff54495
Brown69 <brown.dmitry@gmail.com>
2023-07-02 17:48:22 +0300

    Small changes added1

--- src/exam/RobotFight.java
+++ src/exam/RobotFight.java
@@ -3,8 +3,14 @@ package exam;
 public class RobotFight {
     public static void main(String[] args) {
 
- String robot1 = EnterOfName.enterOfNameFromConsole();
-        System.out.println(robot1);
+        Robots robot1 = new Robots();
+        Robots robot2 = new Robots();
+
+        String robotName1 = EnterOfName.enterOfNameFromConsole();
+        int robotHealth1 = Robots.robotHealth;
+
+        String robotName2 = EnterOfName.enterOfNameFromConsole();
+        int robotHealth2 = Robots.robotHealth;
 
     }
 }
--- src/exam/Robots.java
+++ src/exam/Robots.java
@@ -2,7 +2,7 @@ package exam;
 
 public class Robots {
 
-    public int robotHealth = 100;
+    public static int robotHealth = 100;
 
     public String robotName;
 
